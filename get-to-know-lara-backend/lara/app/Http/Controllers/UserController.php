f1109811a52954a44b8e3adaae8fe9f86fbe73b3
mahanna <mahanna@mahanna.com>
2021-04-21 16:30:45 +0200

    add login and register methods to UserController

--- get-to-know-lara-backend/lara/app/Http/Controllers/UserController.php
+++ get-to-know-lara-backend/lara/app/Http/Controllers/UserController.php
@@ -3,8 +3,91 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Symfony\Component\HttpFoundation\Response;
 
 class UserController extends Controller
 {
-    //
+    function index(Request $request)
+    {
+        try{
+            $validation = $request->validate([
+                'email' => 'required|email',
+                'password' => 'required']);
+        }
+        catch (\Exception $exception){
+            return response()->json("error", Response::HTTP_BAD_REQUEST);
+        }
+
+        $user = User::where('email', $request->email)->first();
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            return response([
+                'message' => ['These credentials do not match our records.']
+            ], Response::HTTP_NOT_FOUND);
+        }
+
+        $token = $user->createToken('my-app-token')->plainTextToken;
+
+        $response = [
+            'user' => $user,
+            'token' => $token
+        ];
+
+//        return response()->json($error, Response::HTTP_BAD_REQUEST);
+
+        return response($response, Response::HTTP_CREATED);
+    }
+
+    public function register(Request $request)
+    {
+        try{
+            $validation = $request->validate([
+                'email' => 'required|email',
+                'password' => 'required']);
+        }
+        catch (\Exception $exception){
+            return response()->json("error", Response::HTTP_BAD_REQUEST);
+        }
+
+        User::firstOrCreate([ "name" => $request->get('name'), "email" => $request->get('email'), "password" => Hash::make( $request->get('password'))]);
+        return response()->json([
+            'status_code' => 200,
+            'message' => 'User created successfully'
+        ]);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
 }
--- get-to-know-lara-backend/lara/app/Models/User.php
+++ get-to-know-lara-backend/lara/app/Models/User.php
@@ -6,6 +6,8 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Database\Eloquent\Model;
 
 
 /**
@@ -15,7 +17,7 @@ use Illuminate\Notifications\Notifiable;
  */
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, HasApiTokens;
 
     /**
      * The attributes that are mass assignable.
