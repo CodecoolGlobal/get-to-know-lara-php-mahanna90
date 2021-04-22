f997d1b9407036d86fff76b883eaf635c9430daa
mahanna <mahanna@mahanna.com>
2021-04-22 12:02:15 +0200

    add login form and alert to homepage

--- get-to-know-lara-frontend/src/components/Home.js
+++ get-to-know-lara-frontend/src/components/Home.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import NavBar from './NavBar';
-import {ThemeProvider, createMuiTheme} from '@material-ui/core/styles';
+import {ThemeProvider, createMuiTheme, makeStyles} from '@material-ui/core/styles';
 import {deepOrange, teal} from "@material-ui/core/colors";
+import MailList from "./MailList";
+import Login from "./Login";
+import {Alert, AlertTitle} from "@material-ui/lab";
+
 
 function Home() {
 
@@ -14,7 +18,18 @@ function Home() {
 
     return (
         <ThemeProvider theme={theme}>
-            <NavBar />
+            <NavBar/>
+            {sessionStorage.getItem('token') ?
+                <MailList/>
+                :
+                <>
+                    <Alert severity="warning" >
+                        <AlertTitle>Warning</AlertTitle>
+                        You are not logged in — <strong>Please sign in to check your mails!</strong>
+                    </Alert>
+                    <Login/>
+                </>
+            }
         </ThemeProvider>
     )
 }
--- get-to-know-lara-frontend/src/components/Login.js
+++ get-to-know-lara-frontend/src/components/Login.js
@@ -1,9 +1,47 @@
 import React, {useState} from "react";
 import axios from 'axios';
 import Spinner from "react-spinner-material";
+import {makeStyles} from "@material-ui/core/styles";
+import {
+    Avatar,
+    Checkbox,
+    Container,
+    CssBaseline,
+    FormControlLabel,
+    Grid,
+    Link,
+    Paper,
+    TextField
+} from "@material-ui/core";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+
+
+const useStyles = makeStyles((theme) => ({
+    paper: {
+        marginTop: theme.spacing(8),
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+    },
+    avatar: {
+        margin: theme.spacing(1),
+        backgroundColor: theme.palette.secondary.main,
+    },
+    form: {
+        width: '100%', // Fix IE 11 issue.
+        marginTop: theme.spacing(1),
+    },
+    submit: {
+        margin: theme.spacing(3, 0, 2),
+    },
+}));
 
 function Login() {
-    const load = { position: "fixed", top: "50%", left: "50%", transform: "translate(-50%, -50%)" };
+    const classes = useStyles();
+
+    const load = {position: "fixed", top: "50%", left: "50%", transform: "translate(-50%, -50%)"};
 
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
@@ -13,13 +51,13 @@ function Login() {
         setLoading(true);
         e.preventDefault();
         axios.post('http://localhost:3000/get-to-know-lara-php-mahanna90/get-to-know-lara-backend/lara/api/login', {
-                headers: {
-                    "Content-Type": "application/json",
-                    "Accepted": "application/json",
-                },
-                email,
-                password,
-            })
+            headers: {
+                "Content-Type": "application/json",
+                "Accepted": "application/json",
+            },
+            email,
+            password,
+        })
             .then((response) => {
                 setLoading(false);
                 sessionStorage.setItem("user", JSON.stringify(response.data.user));
@@ -45,9 +83,67 @@ function Login() {
 
 
     return (
-        <>
+        <Container component="main" maxWidth="xs">
+            <CssBaseline/>
+            <div className={classes.paper}>
+                <Avatar className={classes.avatar}>
+                    <LockOutlinedIcon/>
+                </Avatar>
+                <Typography component="h1" variant="h5">
+                    Sign in
+                </Typography>
+                <form className={classes.form} noValidate>
+                    <TextField
+                        variant="outlined"
+                        margin="normal"
+                        required
+                        fullWidth
+                        id="email"
+                        label="Email Address"
+                        name="email"
+                        autoComplete="email"
+                        autoFocus
+                    />
+                    <TextField
+                        variant="outlined"
+                        margin="normal"
+                        required
+                        fullWidth
+                        name="password"
+                        label="Password"
+                        type="password"
+                        id="password"
+                        autoComplete="current-password"
+                    />
+                    {/*<FormControlLabel*/}
+                    {/*    control={<Checkbox value="remember" color="primary" />}*/}
+                    {/*    label="Remember me"*/}
+                    {/*/>*/}
+                    <Button
+                        type="submit"
+                        fullWidth
+                        variant="contained"
+                        color="primary"
+                        className={classes.submit}
+                    >
+                        Sign In
+                    </Button>
+                    <Grid container>
+                        <Grid item xs>
+                            <Link href="#" variant="body2">
+                                Forgot password?
+                            </Link>
+                        </Grid>
+                        <Grid item>
+                            <Link href="#" variant="body2">
+                                {"Don't have an account? Sign Up"}
+                            </Link>
+                        </Grid>
+                    </Grid>
+                </form>
+            </div>
+        </Container>
 
-        </>
     )
 }
 
