a0a9e83e8e31686cea892c1f697b5d99ee2042a4
mahanna <mahanna@mahanna.com>
2021-04-28 00:12:50 +0200

    implement fetch inbox from frontend with hardcoded test user id

--- get-to-know-lara-frontend/src/components/MailList.js
+++ get-to-know-lara-frontend/src/components/MailList.js
@@ -1,20 +1,51 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react';
+import axios from "axios";
+import {BASE_URL, MESSAGES} from "../Constants";
 
 
 function MailList() {
+    const [mails, setMails] = useState([]);
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        setLoading(true);
+        axios.get(`${BASE_URL}/get-to-know-lara-php-mahanna90/get-to-know-lara-backend/lara/public/api/mails/inbox`,  {
+            withCredentials: true,
+            mode: "cors",
+            headers: {
+                "Content-Type": "application/json",
+                "Accepted": "application/json",
+                'Authorization': `Bearer ${sessionStorage.getItem('token')}`
+            },
+            params: {
+                id: 7,
+            }
+        })
+            .then((res) => {
+                console.log(res);
+                setMails(prevMails => res.data)
+                setLoading(false);
+                console.log(mails);
+            })
+            .catch(function (error) {
+                alert("Error: Cannot fetch emails!");
+                // setMessage(MESSAGES.LOGIN_ERROR_MSG);
+                // sessionStorage.clear();
+            });
+
+        return () => {
+            console.log("unmounted mails/inbox useEffect")
+        }
+    }, []);
+
+
     return (
         <>
-            <h1>YOUR EMAILS</h1>
-            <h1>YOUR EMAILS</h1>
-            <h1>YOUR EMAILS</h1>
-            <h1>YOUR EMAILS</h1>
-            <h1>YOUR EMAILS</h1>
-            <h1>YOUR EMAILS</h1>
-            <h1>YOUR EMAILS</h1>
-            <h1>YOUR EMAILS</h1>
-            <h1>YOUR EMAILS</h1>
+            {mails.map((mail, i) => (
+                <h1 key={i}>{mail.subject}</h1>
+            )) }
         </>
-    )
+    );
 }
 
 export default MailList;
